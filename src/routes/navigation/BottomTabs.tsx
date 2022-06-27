7cf49f5e03de6017532400647e9a5c58741a42ee
FARANDISUTANTO <51822089+Gino-Hariman@users.noreply.github.com>
2022-06-27 21:25:53 +0700

    fix bottom tab icon

--- src/routes/navigation/BottomTabs.tsx
+++ src/routes/navigation/BottomTabs.tsx
@@ -4,18 +4,24 @@ import SeatFill from "@Icons/Fill/Seat.svg";
 import SeatOutlined from "@Icons/Outline/Seat.svg";
 import HistoryFill from "@Icons/Fill/History.svg";
 import HistoryOutlined from "@Icons/Outline/History.svg";
+import AHistoryOutlined from "@Icons/Outline/AHistory.svg";
 import ProfileFill from "@Icons/Fill/Profile.svg";
 import ProfileOutlined from "@Icons/Outline/Profile.svg";
+import AProfileOutlined from "@Icons/Outline/AProfile.svg";
 import HomeFill from "@Icons/Fill/Home.svg";
 import HomeOutlined from "@Icons/Outline/Home.svg";
+import AHomeOutlined from "@Icons/Outline/AHome.svg";
 import HomeStack from "routes/stack/HomeStack";
 import SeatStack from "routes/stack/SeatStack";
 import ProfileStack from "routes/stack/ProfileStack";
 import HistoryStack from "routes/stack/HistoryStack";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
 const BottomTabs = (): JSX.Element => {
+  const isIos = Platform.OS === "ios";
+
   return (
     <Tabs.Navigator
       screenOptions={({ route }) => ({
@@ -25,9 +31,12 @@ const BottomTabs = (): JSX.Element => {
           if (route.name === "My Home")
             return focused ? (
               <HomeFill width={size} height={size} />
-            ) : (
+            ) : isIos ? (
               <HomeOutlined width={size} height={size} />
+            ) : (
+              <AHomeOutlined width={size} height={size} />
             );
+
           if (route.name === "My Seat")
             return focused ? (
               <SeatFill width={size} height={size} />
@@ -37,14 +46,18 @@ const BottomTabs = (): JSX.Element => {
           if (route.name === "My History")
             return focused ? (
               <HistoryFill width={size} height={size} />
-            ) : (
+            ) : isIos ? (
               <HistoryOutlined width={size} height={size} />
+            ) : (
+              <AHistoryOutlined width={size} height={size} />
             );
           if (route.name === "Profile")
             return focused ? (
               <ProfileFill width={size} height={size} />
-            ) : (
+            ) : isIos ? (
               <ProfileOutlined width={size} height={size} />
+            ) : (
+              <AProfileOutlined width={size} height={size} />
             );
           return <></>;
         },
